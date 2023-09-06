2f8b7265af60cc60b918309226694af00d1efe3a
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2023-09-05 20:14:31 -0400

    make it build in dev

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'public',
-			assets: 'public',
+			pages: process.env.NODE_ENV === 'production' ? 'public' : 'build',
+			assets: process.env.NODE_ENV === 'production' ? 'public' : 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true

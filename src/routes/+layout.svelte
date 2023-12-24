886017b1108f96c433df9c3de2a531861c54a39e
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2023-12-24 07:50:52 -0500

    fix hehe

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,20 +6,26 @@
 <slot />
 
 <style>
-	:global(body) {
-		display: contents;
-		font-family: 'Atkinson Hyperlegible', sans-serif;
-		font-size: 32px;
+	:root {
 		--background-color: #fffbf5;
 		--color: #241f31;
-		color: var(--color);
 		--palette-light: #fffbf5;
 		--palette-dark: #241f31;
 		--accent-color: #d4131b;
-
+		
 		@media (prefers-color-scheme: dark) {
 			--background-color: #241f31;
 			--color: #fffbf5;
 		}
+	}
+	
+	:global(body) {
+		font-family: 'Atkinson Hyperlegible', sans-serif;
+		font-size: 32px;
+		color: var(--color);
+	}
+
+	:global(html) {
+		background: var(--background-color);
 	}
 </style>

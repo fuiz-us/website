c4a562221ffa9e15bf70e921fd6e0e284b02e5b8
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2024-01-08 20:56:41 -0500

    maybe fix the issue for

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -4,11 +4,22 @@ import type { RequestHandler } from '@sveltejs/kit';
 export const prerender = true;
 
 export const GET: RequestHandler = async () => {
-	return await sitemap.response({
-		origin: 'https://fuiz.us',
-		lang: {
-			default: 'en',
-			alternates: ['es']
+	let text = await (
+		await sitemap.response({
+			origin: 'https://fuiz.us',
+			lang: {
+				default: 'en',
+				alternates: ['es']
+			}
+		})
+	).text();
+
+	let res = new Response(text.replaceAll('https://www', 'http://www'), {
+		headers: {
+			'cache-control': 'max-age=0, s-maxage=3600',
+			'content-type': 'application/xml'
 		}
 	});
+
+	return res;
 };

f5b15f32e4defa118e55d2848f7120dee32087c6
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2024-01-09 12:59:02 -0500

    figured things out

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -4,7 +4,7 @@ import type { RequestHandler } from '@sveltejs/kit';
 export const prerender = true;
 
 export const GET: RequestHandler = async () => {
-	let text = await (
+	const text = await (
 		await sitemap.response({
 			origin: 'https://fuiz.us',
 			lang: {
@@ -14,15 +14,10 @@ export const GET: RequestHandler = async () => {
 		})
 	).text();
 
-	let res = new Response(
-		text.replace('https://www.s', 'http://www.s').replaceAll('https://www.g', 'http://www.g'),
-		{
-			headers: {
-				'cache-control': 'max-age=0, s-maxage=3600',
-				'content-type': 'text/xml'
-			}
+	return new Response(text.replaceAll('https://www', 'http://www'), {
+		headers: {
+			'cache-control': 'max-age=0, s-maxage=3600',
+			'content-type': 'text/xml'
 		}
-	);
-
-	return res;
+	});
 };

4cf493a80c043854ed9c0a626c97855e25dd88f3
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2024-10-09 11:17:25 -0700

    rewrite confetti logic

--- src/routes/play/Result.svelte
+++ src/routes/play/Result.svelte
@@ -8,79 +8,95 @@
 	import { onMount } from 'svelte';
 
 	onMount(() => {
-		let W = window.innerWidth;
-		let H = window.innerHeight;
-		const canvas = document.getElementById("confetti");
-		const context = canvas.getContext("2d");
-		const maxConfettis = 150;
-		const particles = [];
+		class ConfettiParticle {
+			x: number;
+			y: number;
+			r: number;
+			d: number;
+			color: string;
+			tilt: number;
+			tiltAngleIncremental: number;
+			tiltAngle: number;
 
-		const possibleColors = [
-			"DodgerBlue",
-			"OliveDrab",
-			"Gold",
-			"Pink",
-			"SlateBlue",
-			"LightBlue",
-			"Gold",
-			"Violet",
-			"PaleGreen",
-			"SteelBlue",
-			"SandyBrown",
-			"Chocolate",
-			"Crimson"
-		];
+			constructor() {
+				this.x = Math.random() * W; // x
+				this.y = Math.random() * H - H; // y
+				this.r = randomFromTo(11, 33); // radius
+				this.d = Math.random() * maxConfettis + 11;
+				this.color = possibleColors[Math.floor(Math.random() * possibleColors.length)];
+				this.tilt = Math.floor(Math.random() * 33) - 11;
+				this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
+				this.tiltAngle = 0;
+			}
 
-		function randomFromTo(from, to) {
-			return Math.floor(Math.random() * (to - from + 1) + from);
-		}
-
-		function confettiParticle() {
-			this.x = Math.random() * W; // x
-			this.y = Math.random() * H - H; // y
-			this.r = randomFromTo(11, 33); // radius
-			this.d = Math.random() * maxConfettis + 11;
-			this.color =
-				possibleColors[Math.floor(Math.random() * possibleColors.length)];
-			this.tilt = Math.floor(Math.random() * 33) - 11;
-			this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
-			this.tiltAngle = 0;
-
-			this.draw = function () {
+			draw(context: CanvasRenderingContext2D) {
 				context.beginPath();
 				context.lineWidth = this.r / 2;
 				context.strokeStyle = this.color;
 				context.moveTo(this.x + this.tilt + this.r / 3, this.y);
 				context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
 				return context.stroke();
+			}
+		}
+
+		function getSize(canvas: HTMLCanvasElement) {
+			canvas.width = 0;
+			canvas.height = 0;
+			const confettiContainer = document.querySelector('#confetti-container');
+			return {
+				width: confettiContainer?.clientWidth ?? window.innerWidth,
+				height: confettiContainer?.clientHeight ?? window.innerHeight
 			};
 		}
 
-		function Draw() {
+		const canvas = document.getElementById('confetti') as HTMLCanvasElement | null;
+		if (!canvas) return;
+
+		let { width: W, height: H } = getSize(canvas);
+
+		const context = canvas.getContext('2d');
+		if (!context) return;
+
+		const maxConfettis = 150;
+		const particles: ConfettiParticle[] = [];
+
+		const possibleColors = [
+			'DodgerBlue',
+			'OliveDrab',
+			'Gold',
+			'Pink',
+			'SlateBlue',
+			'LightBlue',
+			'Gold',
+			'Violet',
+			'PaleGreen',
+			'SteelBlue',
+			'SandyBrown',
+			'Chocolate',
+			'Crimson'
+		];
+
+		function randomFromTo(from: number, to: number) {
+			return Math.floor(Math.random() * (to - from + 1) + from);
+		}
+
+		function drawParticles(context: CanvasRenderingContext2D) {
 			const results = [];
 
 			// Magical recursive functional love
-			requestAnimationFrame(Draw);
+			requestAnimationFrame(() => drawParticles(context));
 
 			context.clearRect(0, 0, W, window.innerHeight);
 
-			for (var i = 0; i < maxConfettis; i++) {
-				results.push(particles[i].draw());
+			for (const particle of particles) {
+				results.push(particle.draw(context));
 			}
 
-			let particle = {};
-			let remainingFlakes = 0;
-			for (var i = 0; i < maxConfettis; i++) {
-				particle = particles[i];
-
+			for (const [i, particle] of particles.entries()) {
 				particle.tiltAngle += particle.tiltAngleIncremental;
 				particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
 				particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
 
-				if (particle.y <= H) remainingFlakes++;
-
-				// If a confetti has fluttered out of view,
-				// bring it back to above the viewport and let if re-fall.
 				if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
 					particle.x = Math.random() * W;
 					particle.y = -30;
@@ -92,25 +108,26 @@
 		}
 
 		window.addEventListener(
-			"resize",
-			function () {
-				W = window.innerWidth;
-				H = window.innerHeight;
-				canvas.width = window.innerWidth;
-				canvas.height = window.innerHeight;
+			'resize',
+			() => {
+				const size = getSize(canvas);
+				canvas.width = W = size.width;
+				canvas.height = H = size.height;
 			},
-			false
+			{
+				capture: false
+			}
 		);
 
 		// Push new confetti objects to `particles[]`
-		for (var i = 0; i < maxConfettis; i++) {
-			particles.push(new confettiParticle());
+		for (let i = 0; i < maxConfettis; i++) {
+			particles.push(new ConfettiParticle());
 		}
 
 		// Initialize
 		canvas.width = W;
 		canvas.height = H;
-		Draw();
+		drawParticles(context);
 	});
 
 	export let name: string;
@@ -120,7 +137,7 @@
 
 <div style:height="100%" style:display="flex" style:flex-direction="column">
 	<Topbar {name} {score} />
-	<div style:flex="1">
+	<div style:flex="1" id="confetti-container">
 		<NiceBackground>
 			<div
 				style:height="100%"
@@ -137,7 +154,7 @@
 					<div style:font-weight="bold" style:max-width="10ch" style:text-align="center">
 						{#if correct}
 							{m.thats_correct()}
-							<canvas id="confetti"></canvas>
+							<canvas id="confetti" />
 						{:else}
 							{m.try_again()}
 						{/if}
@@ -149,13 +166,13 @@
 </div>
 
 <style>
-#confetti{
-    overflow-y: hidden;
-    overflow-x: hidden;
-    width: 100%;
-    margin: 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-}
+	#confetti {
+		overflow-y: hidden;
+		overflow-x: hidden;
+		width: 100%;
+		margin: 0;
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
 </style>
